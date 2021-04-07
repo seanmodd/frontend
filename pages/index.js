bdfeea44f0295ab13ee0c831a6b79bd2cc335b7a
Sean <sean@vestaridefair.com>
2021-04-06 17:51:00 -0700

    lets goooo baby

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,1 @@
-import Page from '../components/Page';
-import Pagination from '../components/Pagination';
-import Products from '../components/Products';
-
-export default function IndexPage() {
-  return (
-    <>
-      <Pagination page={1} />
-      <Products />
-      <Pagination page={1} />
-    </>
-  );
-}
+export { default } from './products';
